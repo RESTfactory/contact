6ce09e11ad0dbf7ba25fb917b5f2206539a180c5
mcueto <cueto@live.cl>
2017-05-11 23:48:27 -0300

    Added initial models

---
+++ contact/models.py
@@ -0,0 +1,33 @@
+"""Contact models."""
+from django.db import models
+
+
+class ContactCreator(models.Model):
+    name = models.CharField(max_length=100)
+    contact_name = models.CharField(max_length=100, required=False)
+    contact_cellphone = models.CharField(max_length=15, required=False)
+    contact_email = models.CharField(max_length=254)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ContactType(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ContactInfo(models.Model):
+    name = models.CharField(max_length=100)
+    contact_name = models.CharField(max_length=100, required=False)
+    contact_cellphone = models.CharField(max_length=15, required=False)
+    contact_email = models.CharField(max_length=254)
+    creator = models.ForeignKey(ContactCreator)
+    contact_type = models.ForeignKey(ContactType)
+
+    def __str__(self):
+        return self.name
