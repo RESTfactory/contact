55c9f060eb900ef28d89ec1ed9d81fef9431d871
mcueto <cueto@live.cl>
2017-06-08 01:41:30 -0400

    Minor changes pep8, pep257

--- companies/models.py
+++ companies/models.py
@@ -1,7 +1,10 @@
-from django.conf import settings
+"""API Control models."""
 from django.db import models
 
+
 class App(models.Model):
+    """App model: One Instance per Client(business or App) that connects."""
+
     name = models.CharField(max_length=30)
     api_key = models.CharField(max_length=30)
     enabled = models.BooleanField(default=False)
@@ -12,16 +15,23 @@ class App(models.Model):
     def __str__(self):
         return self.name
 
+
 class OrganizationalUnitType(models.Model):
+    """Groups type for an App(example: department)."""
+
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30, blank=True)
     app = models.ForeignKey(App)
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
+    parent = models.ForeignKey('self', blank=True, null=True,
+                               related_name='children')
 
     def __str__(self):
         return self.name
 
+
 class OrganizationalUnit(models.Model):
+    """Group for an App(example: accounting department)."""
+
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30, blank=True)
     unit_type = models.ForeignKey(OrganizationalUnitType, related_name="units")
